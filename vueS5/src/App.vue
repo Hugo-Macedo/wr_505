69b86335800547b8166e390dda2980a90e9f9527
hugo <macedohugo77@gmail.com>
2023-09-19 12:07:08 +0200

    seance1 finished

--- vueS5/src/App.vue
+++ vueS5/src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Note from './components/Note.vue'
+
+const symfony = 18
+const integration = 16
+const reseau = 13
+const title = "Accueil"
+const paragraphe = "Voici un paragraphe"
 </script>
 
 <template>
@@ -8,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -17,69 +24,81 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
+  <body>
+    <h1>{{ title }}</h1>
+    <!-- <ul>
+      <li>Note sur Symfony : {{ symfony }}</li>
+      <li>Note en intégration : {{ integration }}</li>
+      <li>Note en réseau : {{ reseau }}</li>
+    </ul> -->
+
+    <p>{{ paragraphe }}</p>
+    <Note />
+    
+  </body>
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
   header {
+    max-height: 100vh;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
   }
 
   .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    display: block;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
   }
-}
+
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav a:first-of-type {
+    border: 0;
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      place-items: center;
+      padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+      /* margin: 0 2rem 0 0; */
+      text-align: center;
+    }
+
+    header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+
+    nav {
+      text-align: center;
+      margin-left: -1rem;
+      font-size: 1rem;
+
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
+  }
 </style>
---
+++ vueS5/src/components/Note.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const note1 = 15
+const note2 = 18
+const note3 = 10
+const moyenne = (note1 + note2 + note3) / 3
+const items = ref([{ message: 'première valeur' }, { message: 'deuxième message' }])
+
+const somme = computed(() => {
+    return note1 + note2 + note3
+})
+
+
+</script>
+
+<template>
+    <p>voici les 3 notes : {{ note1 }}, {{ note2 }}, {{ note3 }}<br> ainsi que la moyenne : {{ moyenne }}<br> ainsi que la
+        somme : {{ somme }}</p>
+    <li v-for="item in items">
+        <p> {{ item.message }}</p>
+    </li>
+</template>
